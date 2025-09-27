38cb37b3d85f15c56193c656f415c6712153eeaa
Anshika Dixit <dixitanshika09@gmail.com>
2025-09-27 22:36:38 +0530

    CHANGE --> Printed results

--- test/string_calculator.dart
+++ test/string_calculator.dart
@@ -6,3 +6,10 @@ class StringCalculator{
     return numbers.split(',').map(int.parse).reduce((a, b) => a + b);
   }
 }
+
+void main(){
+  StringCalculator calculator = StringCalculator();
+  print(calculator.add(""));
+  print(calculator.add("1"));
+  print(calculator.add("1,2"));
+}
