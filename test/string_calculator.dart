98ac4da20cdd163e87f723fda02d34f25ca92e04
Anshika Dixit <dixitanshika09@gmail.com>
2025-09-27 22:59:39 +0530

    CHANGE --> Allow the Add method to handle new lines between numbers (instead of commas)

--- test/string_calculator.dart
+++ test/string_calculator.dart
@@ -3,7 +3,9 @@ class StringCalculator{
     if(numbers.isEmpty){
       return 0;
     }
-    return numbers.split(',').map(int.parse).reduce((a, b) => a + b);
+    return 
+     numbers.split(RegExp(r'[,\n]'))
+     .map(int.parse).reduce((a, b) => a + b);
   }
 }
 
@@ -12,4 +14,5 @@ void main(){
   print(calculator.add(""));
   print(calculator.add("1"));
   print(calculator.add("1,2,345,346456457,758678,789890890,23423434,6787979,9809809,45,23,2323,2443,43,45,2,1,324,5,5676,8787,989,8988876765,544545,55,46565,565676763434,33232,323232,3232323"));
+  print(calculator.add("1\n2,3"));
 }
