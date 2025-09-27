ded2a81c0977e8739f1cc1675c426581b56edc24
Anshika Dixit <dixitanshika09@gmail.com>
2025-09-27 22:47:08 +0530

    CHANGE --> Added a print to check the  condition - "Unknown amount of numbers"

--- test/string_calculator.dart
+++ test/string_calculator.dart
@@ -11,5 +11,5 @@ void main(){
   StringCalculator calculator = StringCalculator();
   print(calculator.add(""));
   print(calculator.add("1"));
-  print(calculator.add("1,2"));
+  print(calculator.add("1,2,345,346456457,758678,789890890,23423434,6787979,9809809,45,23,2323,2443,43,45,2,1,324,5,5676,8787,989,8988876765,544545,55,46565,565676763434,33232,323232,3232323"));
 }
