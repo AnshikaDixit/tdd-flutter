c829860d3dce36d7425d2e7eb1250b07966a086e
Anshika Dixit <dixitanshika09@gmail.com>
2025-09-28 10:39:50 +0530

    CHANGE --> Removed prints statements and added testcases

--- test/string_calculator.dart
+++ test/string_calculator.dart
@@ -1,6 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
 class StringCalculator {
+  int _callCount = 0;
+
   int add(String numbers) {
-    if (numbers.isEmpty) {
+    _callCount++;
+    if (numbers.trim().isEmpty) {
       return 0;
     }
 
@@ -11,32 +16,65 @@ class StringCalculator {
       int newlineIndex = numbers.indexOf('\n');
       if (newlineIndex != -1) {
         String customDelimiter = numbers.substring(2, newlineIndex);
-        delimiterPattern = RegExp.escape(customDelimiter); // escape for regex
-        numbers = numbers.substring(newlineIndex + 1); // remove delimiter line
+        delimiterPattern = RegExp.escape(customDelimiter);
+        numbers = numbers.substring(newlineIndex + 1);
       }
     }
 
-    List<int> numbersList =
-        numbers.split(RegExp(delimiterPattern)).map(int.parse).toList();
+    List<int> numbersList = numbers
+        .split(RegExp(delimiterPattern))
+        .map(int.parse)
+        .toList();
 
     List<int> negatives = numbersList.where((n) => n < 0).toList();
     if (negatives.isNotEmpty) {
-      print("Negatives not allowed : ${negatives.join(', ')}");
-      throw Exception("Negatives not allowed : ${negatives.join(', ')}");
+      throw Exception("Negatives not allowed: ${negatives.join(', ')}");
     }
 
-    return numbersList.reduce((a, b) => a + b);
+    return numbersList.isEmpty ? 0 : numbersList.reduce((a, b) => a + b);
+  }
+
+  int getCalledCount() {
+    return _callCount;
   }
 }
 
 void main() {
-  StringCalculator calculator = StringCalculator();
-  print(calculator.add(""));
-  print(calculator.add("1"));
-  print(calculator.add(
-      "1,2,345,346456457,758678,789890890,23423434,6787979,9809809,45,23,2323,2443,43,45,2,1,324,5,5676,8787,989,8988876765,544545,55,46565,565676763434,33232,323232,3232323"));
-  print(calculator.add("1\n2,3"));
-  print(calculator.add("//;\n1;2"));
-  print(calculator.add("//;\n-1;-2;-3"));
-  print(calculator.add("//;\n-1;-2;-3;-4;5665;7657678;8768778997;9789789"));
+  test('returns 0 for empty string', () {
+    final calc = StringCalculator();
+    expect(calc.add(""), 0);
+  });
+
+  test('sums comma separated numbers', () {
+    final calc = StringCalculator();
+    expect(calc.add("1,2,3"), 6);
+  });
+
+  test('handles newlines as delimiters', () {
+    final calc = StringCalculator();
+    expect(calc.add("1\n2,3"), 6);
+  });
+
+  test('supports custom delimiter', () {
+    final calc = StringCalculator();
+    expect(calc.add("//;\n1;2;3"), 6);
+  });
+
+  test('throws on negatives and shows all of them', () {
+    final calc = StringCalculator();
+    expect(
+      () => calc.add("//;\n-1;-2;-3"),
+      throwsA(predicate((e) =>
+          e is Exception &&
+          e.toString().contains("Negatives not allowed: -1, -2, -3"))),
+    );
+  });
+
+  test('tracks how many times add() was called', () {
+    final calc = StringCalculator();
+    expect(calc.getCalledCount(), 0);
+    calc.add("1,2");
+    calc.add("3,4");
+    expect(calc.getCalledCount(), 2);
+  });
 }
